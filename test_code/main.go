31e3108bbe4775415bc1c7d2deadae5cf361a932
kuromarimo555 <kuromarimo.555@gmail.com>
2017-09-06 00:20:09 +0900

    オプションでファイル指定から文字コード変換

--- test_code/main.go
+++ test_code/main.go
@@ -1,16 +1,29 @@
 package main
 
-import "os"
-import "fmt"
-import "bufio"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
+)
+
+var (
+	filepath = flag.String("f", "", "filepash set")
+)
 
 func main() {
-	file, err := os.Open("input.txt")
+	flag.Parse()
+
+	file, err := os.Open(*filepath)
 	if err != nil {
 		fmt.Println("ファイル無し")
 		os.Exit(1)
 	}
-	scnr := bufio.NewScanner(file)
+	scnr := bufio.NewScanner(transform.NewReader(file, japanese.ShiftJIS.NewDecoder()))
+
 	for scnr.Scan() {
 		fmt.Println(scnr.Text())
 	}
